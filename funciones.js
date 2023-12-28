37c4f2c8527bb892aefae316349ade82b47fc05b
pabloriveraveras <pablo.rivera.pf@gmail.com>
2023-12-28 17:02:37 -0300

    first commit

---
+++ funciones.js
@@ -0,0 +1,9 @@
+alert ("Bienvenidx")
+var nombre = prompt ("Ingresa tu nombre")
+alert ("Vamos a calcular tu imc")
+var edad = prompt ("Edad (años)")
+var altura = prompt ("Altura (mts)")
+var peso = prompt ("Peso (kg)")
+var color = prompt ("Color Favorito")
+var IMC = (peso/(altura * altura))
+
---
+++ index.js
@@ -0,0 +1,7 @@
+var modo = confirm ( "¿fondo oscuro?")
+
+if (modo) {
+    var java = document.getElementById("todo")
+
+    todo.style.backgroundColor = "black"
+}
